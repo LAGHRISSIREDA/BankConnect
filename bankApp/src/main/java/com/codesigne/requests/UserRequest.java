5c6da1b611fb146f3947b19d29817a46c9c6d1df
Sillfer <gliouinemahdi0@gmail.com>
2022-12-19 16:37:01 +0100

    db

--- bankApp/src/main/java/com/codesigne/requests/UserRequest.java
+++ bankApp/src/main/java/com/codesigne/requests/UserRequest.java
@@ -1,75 +1,90 @@
 package com.codesigne.requests;
 
 public class UserRequest {
-	
-	private String firstname;
-	private String lastname;
-	private String adress;
-	private String email;
-	private String phone;
-	private Double money;
-	private String typecompte;
-	private String status;
-	private String password;
-	
-	//getters and setters==================================================
-	
-	public String getFirstname() {
-		return firstname;
-	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	public String getLastname() {
-		return lastname;
-	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-	public String getAdress() {
-		return adress;
-	}
-	public void setAdress(String adress) {
-		this.adress = adress;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public Double getMoney() {
-		return money;
-	}
-	public void setMoney(Double money) {
-		this.money = money;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public String getTypecompte() {
-		return typecompte;
-	}
-	public void setTypecompte(String typecompte) {
-		this.typecompte = typecompte;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
-	
+
+    private String firstname;
+    private String lastname;
+    private String adress;
+    private String email;
+    private String phone;
+    private Double money;
+    private String typecompte;
+    private String status;
+    private String password;
+
+    //getters and setters==================================================
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTypecompte() {
+        return typecompte;
+    }
+
+    public void setTypecompte(String typecompte) {
+        this.typecompte = typecompte;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 
 }
